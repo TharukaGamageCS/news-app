a89d04c71cedc87dd5fa202c6ec28367f7d9b117
tharukaGamageCS <tharuka@ceylonsolutions.com>
2022-09-15 16:39:29 +0530

    login is available, login

--- app/src/main/java/com/example/newsapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/activities/MainActivity.kt
@@ -2,15 +2,27 @@ package com.example.newsapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.newsapp.R
 import com.example.newsapp.databinding.ActivityMainBinding
+import com.example.newsapp.main.MainViewModel
+import com.example.newsapp.main.MainViewModelFactory
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var factory: MainViewModelFactory
+    lateinit var viewModel: MainViewModel
+    lateinit var navController:NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +30,31 @@ class MainActivity : AppCompatActivity() {
         val view = binding.parent
         setContentView(view)
 
-        setupNavigation()
+        initViewModel()
+        getLoginStatus()
 
+    }
+
+    private fun initViewModel(){
+        viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
+    }
+
+    private fun getLoginStatus(){
+        viewModel.loginStatus.observe(this as LifecycleOwner){ result ->
+            setupNavigation()
+            result.getOrNull()?.also { loggedIn->
+                if (loggedIn){
+                    navController.navigate(R.id.homeFragment)
+                }else{
+                    navController.navigate(R.id.loginFragment)
+                }
+            }
+        }
     }
 
     private fun setupNavigation(){
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
     }
 
 }
--- app/src/test/java/com/example/newsapp/main/MainRepositoryShould.kt
+++ app/src/test/java/com/example/newsapp/main/MainRepositoryShould.kt
@@ -2,7 +2,9 @@ package com.example.newsapp.main
 
 import com.example.newsapp.BaseUnitTest
 import com.example.newsapp.data.local.UserDao
-import kotlinx.coroutines.flow.firstOrNull
+import com.example.newsapp.data.models.User
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 import org.mockito.Mockito.*
@@ -12,14 +14,34 @@ class MainRepositoryShould: BaseUnitTest() {
     private lateinit var repository:MainRepository
     private val userDao:UserDao = mock(UserDao::class.java)
 
+    private val loggedInUsers:List<User> = mock(listOf<User>()::class.java)
+
     @Test
-    fun callUserDaoGetAll() = runTest {
-
+    fun callUserDaoGetAllLoggedIn() = runTest {
         repository = MainRepository(userDao)
-
-        repository.getLoginStatus().firstOrNull()
+        repository.getLoginStatus().first()
         verify(userDao, times(1)).getAll()
+    }
 
+    @Test
+    fun emitTrueIfLoggedIn() = runTest {
+        repository = MainRepository(userDao)
+        `when`(userDao.getAll()).thenReturn(loggedInUsers)
+        assertEquals(true, repository.getLoginStatus().first().getOrNull())
+    }
+
+    @Test
+    fun emitFalseIfNotLoggedIn() = runTest {
+        repository = MainRepository(userDao)
+        `when`(userDao.getAll()).thenReturn(emptyList())
+        assertEquals(false, repository.getLoginStatus().first().getOrNull())
+    }
+
+    @Test
+    fun throwsErrorOnException() = runTest {
+        repository = MainRepository(userDao)
+        `when`(userDao.getAll()).thenReturn(null)
+        assertEquals("Something went wrong", repository.getLoginStatus().first().exceptionOrNull()?.message)
     }
 
 }
